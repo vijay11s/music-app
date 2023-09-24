ba2c63a088a5d70602f0b413941ab9d29941004d
Vijay Sah <vijay.11sah@gmail.com>
2023-09-24 13:14:57 +0530

    creates layout for the page

---
+++ components/Library.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const Library = () => {
+  const onClick = () => {
+    // handle upload
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 py-4">
+        <div className="inline-flex items-center gap-x-2 text-neutral-400">
+          <TbPlaylist size={26} />
+          <p className="font-medium text-md">Your Library</p>
+        </div>
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
+      <div className="flex flex-col gap-y-2 px-3 mt-4">List of songs</div>
+    </div>
+  );
+};
+
+export default Library;
